46af0fbddb15ff8fea9198b33c5d49e4d76eb0bd
hwiseo <gnltj37@naver.com>
2024-05-16 19:35:22 +0900

    init

---
+++ flow/src/test/java/com/hwiseo/flow/FlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwiseo.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
